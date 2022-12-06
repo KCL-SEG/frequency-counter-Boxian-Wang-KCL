93fee9678cf4247af16f9b2d16613cafb05820b8
Boxian Wang <101829608+Boxian-Wang-KCL@users.noreply.github.com>
2022-12-06 21:14:03 +0000

    Update frequencies.py
    update

--- frequencies.py
+++ frequencies.py
@@ -4,8 +4,8 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
-   print("test")
-    for item in items:
-        frequencies[str(item)] = frequencies.get(str(item), 0) + 1
+   
+    for inputs in items:
+        frequencies[str(inputs)] = frequencies.get(str(inputs), 0) + 1
    
     return frequencies
